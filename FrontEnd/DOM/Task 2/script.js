22f9936d40b093854237013188ebcdaf30aa51c6
DibyanshuChauhan <cdivyanshu98@gmail.com>
2025-11-17 19:46:06 +0530

    Task 2 completed of DOM

---
+++ FrontEnd/DOM/Task 2/script.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("button")
+let box = document.getElementById("box")
+
+btn.addEventListener("click", function () {
+    let color1 = Math.floor(Math.random() * 256)
+    let color2 = Math.floor(Math.random() * 256)
+    let color3 = Math.floor(Math.random() * 256)
+
+    box.style.backgroundColor = `rgb(${color1},${color2},${color3})`
+})
