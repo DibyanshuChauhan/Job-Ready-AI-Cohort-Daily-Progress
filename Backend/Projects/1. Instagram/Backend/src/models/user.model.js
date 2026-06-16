af9f293dd6cfae958794f088173f3eda012e740e
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-06-16 22:16:05 +0530

    feat: initialize backend with user authentication
    - created package.json with dependencies
    - added server.js to start the application
    - implemented app.js to set up Express and routes
    - configured MongoDB connection in db.js
    - added user model for MongoDB
    - created auth controller for user registration and login
    - set up authentication routes

---
+++ Backend/Projects/1. Instagram/Backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "UserName already exists."],
+        required: [true, "UserName is required."]
+    },
+    email: {
+        type: String,
+        unique: [true, "With this email id user account already exists."],
+        required: [true, "Email is required."]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."]
+    },
+    bio: String,
+    profileImage: {
+        type: String,
+        default: "https://ik.imagekit.io/4dxhabhfm/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-illustration-vector.webp"
+    }
+})
+
+const userModel = mongoose.model("users", userSchema)
+
+module.exports = userModel
