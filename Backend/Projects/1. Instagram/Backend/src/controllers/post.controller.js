02933ce8328654ca0edbe1e015b0c4c336213b45
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-06-18 13:11:39 +0530

    feat: add get post and get post details functionality with JWT authentication

--- Backend/Projects/1. Instagram/Backend/src/controllers/post.controller.js
+++ Backend/Projects/1. Instagram/Backend/src/controllers/post.controller.js
@@ -52,4 +52,76 @@ const createPostController = async (req, res) => {
     });
 }
 
-module.exports = { createPostController };
+const getPostController = async (req, res) => {
+    const token = req.cookies.token
+
+    let decoded = null
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message: "Token invalid."
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        success: "true",
+        message: "Posts fetched successfully.",
+        posts
+    })
+}
+
+const getPostDetailsController = async (req, res) => {
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized access."
+        })
+    }
+
+    let decoded = null
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid token."
+        })
+    }
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById( postId )
+
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden Content."
+        })
+    }
+
+    return res.status(200).json({
+        success: "true",
+        message: "Post fetched successfully.",
+        post
+    })
+}
+
+module.exports = {
+    createPostController,
+    getPostController,
+    getPostDetailsController
+};
--- Backend/Projects/1. Instagram/Backend/src/routes/post.routes.js
+++ Backend/Projects/1. Instagram/Backend/src/routes/post.routes.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const multer = require("multer")
 
-const { createPostController } = require("../controllers/post.controller")
+const { createPostController, getPostController, getPostDetailsController } = require("../controllers/post.controller")
 
 const upload = multer({ storage: multer.memoryStorage() })
 
 const postRouter = express.Router()
 
 postRouter.post("/", upload.single("image"), createPostController)
+
+postRouter.get("/", getPostController)
+
+postRouter.get("/details/:postId", getPostDetailsController)
 
 module.exports = postRouter
