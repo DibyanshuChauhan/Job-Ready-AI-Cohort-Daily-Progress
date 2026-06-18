0f6dfe28a7fefb71fbfe6a4a3bf870807d0f4f41
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-06-18 11:32:36 +0530

    feat: implement post creation functionality with JWT authentication and image upload

--- Backend/Projects/1. Instagram/Backend/src/controllers/post.controller.js
+++ Backend/Projects/1. Instagram/Backend/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
+const postModel = require("../models/post.model");
 
 const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
@@ -9,17 +11,43 @@ const imagekit = new ImageKit({
 
 const createPostController = async (req, res) => {
 
+    const token = req.cookies.token
+
+    if (!token) {
+        res.status(401).json({
+            message: "Token not provided, Unauthorized access."
+        })
+    }
+
+    let decoded = null
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        res.status(401).json({
+            message: "User not authorized."
+        })
+    }
+
     const uploadedFile = await imagekit.files.upload({
         file: await toFile(
             req.file.buffer,
             req.file.originalname
         ),
         fileName: req.file.originalname,
+        folder: "/Insta-Project"
     });
+
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: uploadedFile.url,
+        user: decoded.id
+    })
 
     return res.status(201).json({
         success: true,
-        message: "Image uploaded successfully",
+        message: "Post created successfully",
+        post,
         data: uploadedFile,
     });
 }
