c214897cd3b2f15b0e98588900e5b754e1b50294
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-06-23 14:45:47 +0530

    feat: add profile route and component to user navigation

--- Backend/Projects/1. Instagram/Frontend/src/routes.jsx
+++ Backend/Projects/1. Instagram/Frontend/src/routes.jsx
@@ -4,21 +4,20 @@ import Register from "./features/auth/pages/Register";
 import Feed from "./features/post/pages/Feed";
 import CreatePost from "./features/post/pages/CreatePost";
 
-export const routes = createBrowserRouter([
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/register",
-        element: <Register />,
-    },
-    {
-        path: "/",
-        element: <Feed />
-    },
-    {
-        path: "/create-post",
-        element: <CreatePost />
-    }
+export const routes = createBrowserRouter([{
+    path: "/register",
+    element: <Register />,
+},
+{
+    path: "/login",
+    element: <Login />,
+},
+{
+    path: "/",
+    element: <Feed />
+},
+{
+    path: "/create-post",
+    element: <CreatePost />
+},
 ]);
