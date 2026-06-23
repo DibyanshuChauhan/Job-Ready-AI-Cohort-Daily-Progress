230a61fb12ec4eaeb7ddc69ac2aa0912e31f7c23
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-06-23 15:35:32 +0530

    Implement code changes to enhance functionality and improve performance

--- Backend/Projects/1. Instagram/Frontend/src/features/post/services/post.api.js
+++ Backend/Projects/1. Instagram/Frontend/src/features/post/services/post.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://job-ready-ai-cohort-daily-progress-2.onrender.com/api/posts",
+    baseURL: "https://job-ready-ai-cohort-daily-progress-2.onrender.com",
     withCredentials: true
 })
 
