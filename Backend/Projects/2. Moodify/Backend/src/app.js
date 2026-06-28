ec1f95142e391407a4d93fad1c4b7a59a9e75f1f
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-06-28 23:48:10 +0530

    fix: import 'path' module in app.js for file path handling

--- Backend/Projects/2. Moodify/Backend/src/app.js
+++ Backend/Projects/2. Moodify/Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path")
 
 const authRouter = require("./routes/auth.routes");
 const songRouter = require("./routes/song.routes");
