ed3e0367fc606e0418f84a4e73b1f58fcfb26074
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-06-28 23:43:04 +0530

    fix: update API base URLs to point to the production server

--- Backend/Projects/2. Moodify/Backend/src/app.js
+++ Backend/Projects/2. Moodify/Backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://job-ready-ai-cohort-daily-progress-2.onrender.com",
     credentials: true,
   }),
 );
--- Backend/Projects/2. Moodify/Frontend/src/Features/Home/service/song.api.js
+++ Backend/Projects/2. Moodify/Frontend/src/Features/Home/service/song.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/songs",
+    baseURL: "https://job-ready-ai-cohort-daily-progress-2.onrender.com/api/songs",
     withCredentials: true
 })
 
