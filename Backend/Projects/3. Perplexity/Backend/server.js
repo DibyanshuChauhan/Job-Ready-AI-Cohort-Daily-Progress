251fa6c017bad83ae8fe472a67205294f4e86665
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-07-02 00:43:04 +0530

    feat: initialize backend with Express, MongoDB, and user authentication models
    - Added package.json with dependencies for Express, Mongoose, and other utilities.
    - Created server.js to set up the Express server and connect to MongoDB.
    - Implemented app.js for middleware configuration including JSON parsing and cookie handling.
    - Established database connection logic in db.js.
    - Defined Chat, Message, and User models with Mongoose schemas and methods for password hashing and comparison.

---
+++ Backend/Projects/3. Perplexity/Backend/server.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import app from "./src/app.js";
+import dns from "dns"
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import connectToDb from "./src/config/db.js";
+
+const PORT = process.env.PORT || 8000;
+
+connectToDb()
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+})
