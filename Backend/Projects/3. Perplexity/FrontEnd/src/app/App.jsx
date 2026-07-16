edcde16a5476f085bb3ccdd022121bcbfdc388cc
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-07-17 01:42:54 +0530

    feat: add app.css for text selection styling and improve user experience

--- Backend/Projects/3. Perplexity/FrontEnd/src/app/App.jsx
+++ Backend/Projects/3. Perplexity/FrontEnd/src/app/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app.routes";
 import { useAuth } from "../features/auth/hook/useAuth";
 import { useEffect } from "react";
+import "./app.css";
 
 const App = () => {
 const auth = useAuth();
