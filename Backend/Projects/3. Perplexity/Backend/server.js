81eef680e91810d252e14cf44bd388f53450adda
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-07-16 09:39:27 +0530

    refactor: remove socket.io integration and simplify server setup

--- Backend/Projects/3. Perplexity/Backend/server.js
+++ Backend/Projects/3. Perplexity/Backend/server.js
@@ -3,14 +3,9 @@ import app from "./src/app.js";
 import dns from "dns"
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import connectToDb from "./src/config/db.js";
-import http from "http";
-import { initSocket } from "./src/sockets/server.socket.js";
-
-const httpServer = http.createServer(app);
-initSocket(httpServer);
 
 connectToDb()
 
-httpServer.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is running on port " + process.env.PORT)
 })
