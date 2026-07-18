fc66bec516a64867aedd4e340ee134d65a7d1070
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-07-18 12:12:11 +0530

    feat: add DNS server configuration for improved network resolution

--- Backend/Projects/3. Perplexity/Backend/server.js
+++ Backend/Projects/3. Perplexity/Backend/server.js
@@ -2,6 +2,9 @@ import "dotenv/config";
 import app from "./src/app.js";
 import connectToDb from "./src/config/db.js";
 
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 connectToDb();
 
 app.listen(process.env.PORT || 3000, () => {
