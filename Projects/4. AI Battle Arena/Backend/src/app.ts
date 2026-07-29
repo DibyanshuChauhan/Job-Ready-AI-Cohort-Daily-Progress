e15e98f8b14962150322e9e052dd069bcdd12c7b
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-07-29 23:17:07 +0530

    feat: update dependencies and enhance graph service with new AI model integration

--- Projects/4. AI Battle Arena/Backend/src/app.ts
+++ Projects/4. AI Battle Arena/Backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import useGraph from "./services/graph.ai.service.js"
 
 const app = express();
 
@@ -6,5 +7,9 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello World!...");
 })
+
+app.post("/use-graph", async (req, res) => {
+    await useGraph("What is the capital of India?") 
+});
 
 export default app;
--- Projects/4. AI Battle Arena/Backend/src/services/graph.ai.service.ts
+++ Projects/4. AI Battle Arena/Backend/src/services/graph.ai.service.ts
@@ -1,25 +1,84 @@
-import { StateSchema, MessagesValue, type GraphNode, StateGraph, START, END } from "@langchain/langgraph";
+import { HumanMessage } from "@langchain/core/messages";
+import { StateSchema, MessagesValue, ReducedValue, StateGraph, START, END } from "@langchain/langgraph";
+import type { GraphNode } from "@langchain/langgraph";
+import { z } from "zod";
+import { CohereModel, MistralModel, GeminiModel } from "./ai.models.service.js";
+import { createAgent, providerStrategy } from "langchain";
 
-type JUDGEMENT = {
-    winner: "solution_1" | "solution_2";
-    solution_1_score: number;
-    solution_2_score: number;
-}
-
-type AI_Battle_State = {
-    messages: typeof MessagesValue;
-    solution_1: string;
-    solution_2: string;
-    judgement: JUDGEMENT;
-}
-
-const state:AI_Battle_State = {
+const State = new StateSchema({
     messages: MessagesValue,
-    solution_1: "",
-    solution_2: "",
-    judgement: {
-        winner: "solution_1",
+    solution_1: new ReducedValue(z.string().default(""), {
+        reducer: (current, next) => {
+            return next
+        }
+    }),
+    solution_2: new ReducedValue(z.string().default(""), {
+        reducer: (current, next) => {
+            return next
+        }
+    }),
+    judge_recommendation: new ReducedValue(z.object().default({
         solution_1_score: 0,
-        solution_2_score: 0
+        solution_2_score: 0,
+    }),
+        {
+            reducer: (current, next) => {
+                return next
+            }
+        }
+    )
+})
+
+const solutionNode: GraphNode<typeof State> = async (state: typeof State) => {
+    console.log(state)
+    const [mistral_solution, cohere_solution] = await Promise.all([
+        MistralModel.invoke(state.messages[0].text),
+        CohereModel.invoke(state.messages[0].text)
+    ])
+    return {
+        solution_1: mistral_solution.text,
+        solution_2: cohere_solution.text,
     }
+}
+
+const judgeNode: GraphNode<typeof State> = async (state: typeof State) => {
+    console.log('invoking judge with state,' ,state)
+    const { solution_1, solution_2 } = state;
+    const judge = createAgent({
+        model: GeminiModel,
+        tools: [],
+        responseFormat: providerStrategy(z.object({
+            solution_1_score: z.number().min(0).max(10),
+            solution_2_score: z.number().min(0).max(10),
+        }))
+    })
+    const judge_response = await judge.invoke({
+        messages: [
+            new HumanMessage(
+                `You are a judge for an AI battle. You have two solutions to evaluate in between 0 and 10, where 0 is the worst and 10 is the best. the first solution is: ${solution_1}. The second solution is: ${solution_2}. Please provide your evaluation`
+            )
+        ]
+    })
+    const result = judge_response.structuredResponse
+    return {
+        judge_recommendation: result
+    }
+}
+
+const graph = new StateGraph(State)
+    .addNode("solution", solutionNode)
+    .addNode("judge", judgeNode)
+    .addEdge(START, "solution")
+    .addEdge("solution", "judge")
+    .addEdge("solution", END)
+    .compile();
+
+export default async function (userMessage: string) {
+    const result = await graph.invoke({
+        messages: [
+            new HumanMessage(userMessage)
+        ]
+    })
+    console.log(result)
+    return result.messages
 }
