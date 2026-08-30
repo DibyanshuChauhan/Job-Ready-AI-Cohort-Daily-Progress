a388b4598bfafb6033ea5be8e15faf28300d9099
DibyanshuChauhan <cdivyanshu98@gmail.com>
2026-08-30 12:53:15 +0530

    feat: initialize React frontend project with Vite, Tailwind CSS, and React Router routing

--- Projects/4. Snitch/Frontend/src/app/App.jsx
+++ Projects/4. Snitch/Frontend/src/app/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { RouterProvider } from 'react-router';
+import { router } from './app.routes';
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!.</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
